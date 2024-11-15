3c491740fcc0b6e1eeaa8b794b00f4d88be294c5
PaulMorris001 <Odemuyiwaoladimeji2019@gmail.com>
2024-11-15 16:44:40 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+    },
+  },
+  plugins: [],
+},
+}
+
